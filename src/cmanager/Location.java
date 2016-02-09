75df66b4c88e6ec6422f42be50e670a54c403399
RoffelKartoffel <RoffelKartoffel@users.noreply.github.com>
2016-02-09 02:05:21 +0100

    Partial support for named geo locations

---
+++ src/cmanager/Location.java
@@ -0,0 +1,27 @@
+package cmanager;
+
+public class Location extends Coordinate 
+{
+	private static final long serialVersionUID = 1L;
+
+	private String name;
+	
+	public Location(String name, double lat, double lon) throws Exception {
+		super(lat, lon);
+		setName(name);
+	}
+	
+	public void setName(String name) throws Exception
+	{
+		name = name.trim();
+		if( name.equals("") )
+			throw new Exception("Name must not be empty.");
+		
+		this.name = name;
+	}
+	
+	public String getName(){
+		return name;
+	}
+
+}

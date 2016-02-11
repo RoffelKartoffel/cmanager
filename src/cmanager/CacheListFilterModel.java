7cc2bccab9cb03832f9e8fa10a4ebb3c93163b70
RoffelKartoffel <RoffelKartoffel@users.noreply.github.com>
2016-02-12 00:51:10 +0100

    Preserve constant order during filtering to enable sorting

--- src/cmanager/CacheListFilterModel.java
+++ src/cmanager/CacheListFilterModel.java
@@ -9,7 +9,7 @@ public abstract class CacheListFilterModel extends CacheListFilter
 	
 	public ArrayList<Geocache> getFiltered(final ArrayList<Geocache> originalList)
 	{
-		final ArrayList<Geocache> list = new ArrayList<>();
+		
 //		for( Geocache g : originalList )
 //			if( (!inverted && isGood(g)) || (inverted && !isGood(g)) )
 //				list.add(g);
@@ -19,9 +19,13 @@ public abstract class CacheListFilterModel extends CacheListFilter
 		int cores = ts.getCores(listSize);
 		int perProcess = listSize / cores;
 		
+		final ArrayList<ArrayList<Geocache>> lists = new ArrayList<>(5);
+		for(int c=0; c<cores; c++)
+			lists.add( new ArrayList<Geocache>() );
 		for(int c=0; c<cores; c++)
 		{
 			final int start = perProcess*c;
+			final int c_final = c;
 			
 			int tmp = perProcess*(c+1) < listSize ? perProcess*(c+1) : listSize;
 			if( c == cores -1 )
@@ -31,14 +35,13 @@ public abstract class CacheListFilterModel extends CacheListFilter
 			ts.addAndRun( new Thread(new Runnable(){
 				public void run() 
 				{
+					ArrayList<Geocache> list = lists.get(c_final);
 					try {
 						for(int i=start; i<end; i++)
 						{
 							Geocache g = originalList.get(i);
 							if( (!inverted && isGood(g)) || (inverted && !isGood(g)) )
-								synchronized(originalList) {
 									list.add(g);
-								}
 						}
 					}catch(Throwable ex){
 						Thread t = Thread.currentThread();
@@ -50,12 +53,14 @@ public abstract class CacheListFilterModel extends CacheListFilter
 		try {
 			ts.joinAndThrow();
 		} catch (Throwable e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		
+		ArrayList<Geocache> list_final = new ArrayList<>();
+		for(ArrayList<Geocache> list : lists )
+			list_final.addAll(list);
 		
-		return list;
+		return list_final;
 	}
 	
 	abstract protected boolean isGood(Geocache g);

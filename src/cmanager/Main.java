29a759ba78662d81b49fa8fab4af67acca8d9a43
RoffelKartoffel <RoffelKartoffel@users.noreply.github.com>
2016-02-11 20:11:11 +0100

    release 0.2s

--- src/cmanager/Main.java
+++ src/cmanager/Main.java
@@ -35,7 +35,7 @@ import org.joda.time.DateTimeZone;
 public class Main 
 {
 	public static final String APP_NAME = "cmanager";
-	public static final String VERSION = "0.2r2";
+	public static final String VERSION = "0.2s";
 	
 	private static final String JAR_NAME = "cm.jar";
 	private static final String PARAM_HEAP_RESIZED = "resized";

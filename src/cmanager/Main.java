36823beee384a9d3663af80c79fb36fa6946729c
RoffelKartoffel <RoffelKartoffel@users.noreply.github.com>
2016-02-24 23:53:21 +0100

    version

--- src/cmanager/Main.java
+++ src/cmanager/Main.java
@@ -16,7 +16,7 @@ import javax.swing.JOptionPane;
 public class Main 
 {
 	public static final String APP_NAME = "cmanager";
-	public static final String VERSION = "0.2x";
+	public static final String VERSION = "0.2y";
 	
 	private static final String JAR_NAME = "cm.jar";
 	private static final String PARAM_HEAP_RESIZED = "resized";

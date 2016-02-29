72387f0640661673f3f2846a65f554bf0d7d9565
RoffelKartoffel <RoffelKartoffel@users.noreply.github.com>
2016-02-29 22:44:39 +0100

    version

--- src/cmanager/Main.java
+++ src/cmanager/Main.java
@@ -16,7 +16,7 @@ import javax.swing.JOptionPane;
 public class Main 
 {
 	public static final String APP_NAME = "cmanager";
-	public static final String VERSION = "0.2.27";
+	public static final String VERSION = "0.2.28";
 	
 	public static final String CACHE_FOLDER = System.getProperty("user.home") + "/." + Main.APP_NAME + "/cache/";
 	

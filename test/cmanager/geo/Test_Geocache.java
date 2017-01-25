1fa7097ff320a8ddfe6b8b16bbdd538cb3801504
RoffelKartoffel <RoffelKartoffel@users.noreply.github.com>
2017-01-25 19:43:20 +0100

    Add tests for Geocache

---
+++ test/cmanager/geo/Test_Geocache.java
@@ -0,0 +1,104 @@
+package cmanager.geo;
+
+import org.junit.Test;
+
+import cmanager.ObjectHelper;
+
+import static org.junit.Assert.*;
+
+public class Test_Geocache
+{
+    @Test public void testConstructor()
+    {
+
+        new Geocache("OC1234", "test", new Coordinate(0, 0), 0.0, 0.0, "Tradi");
+
+        try
+        {
+            new Geocache(null, "test", new Coordinate(0, 0), 0.0, 0.0, "Tradi");
+            fail("Exception expected");
+        }
+        catch (NullPointerException e)
+        {
+        }
+
+        try
+        {
+            new Geocache("OC1234", null, new Coordinate(0, 0), 0.0, 0.0,
+                         "Tradi");
+            fail("Exception expected");
+        }
+        catch (NullPointerException e)
+        {
+        }
+
+        try
+        {
+            new Geocache("OC1234", "test", null, 0.0, 0.0, "Tradi");
+            fail("Exception expected");
+        }
+        catch (NullPointerException e)
+        {
+        }
+
+        try
+        {
+            new Geocache("OC1234", "test", new Coordinate(0, 0), null, 0.0,
+                         "Tradi");
+            fail("Exception expected");
+        }
+        catch (NullPointerException e)
+        {
+        }
+
+        try
+        {
+            new Geocache("OC1234", "test", new Coordinate(0, 0), 0.0, null,
+                         "Tradi");
+            fail("Exception expected");
+        }
+        catch (NullPointerException e)
+        {
+        }
+
+        try
+        {
+            new Geocache("OC1234", "test", new Coordinate(0, 0), 0.0, 0.0,
+                         null);
+            fail("Exception expected");
+        }
+        catch (NullPointerException e)
+        {
+        }
+    }
+
+    @Test public void testDataInterpretation()
+    {
+        Geocache g;
+        g = new Geocache("OC1234", "test", new Coordinate(0, 0), 0.0, 0.0,
+                         "Tradi");
+        assertTrue(g.isOC());
+        assertFalse(g.isGC());
+        assertFalse(g.hasVolatileStart());
+        assertEquals(g.getURL(), "https://www.opencaching.de/OC1234");
+
+        g = new Geocache("GC1234", "test", new Coordinate(0, 0), 0.0, 0.0,
+                         "Tradi");
+        assertTrue(g.isGC());
+        assertFalse(g.isOC());
+        assertFalse(g.hasVolatileStart());
+        assertEquals(g.getURL(), "https://www.geocaching.com/geocache/GC1234");
+
+        g = new Geocache("OC1234", "test", new Coordinate(0, 0), 0.0, 0.0,
+                         "Mystery");
+        assertTrue(g.hasVolatileStart());
+    }
+
+    @Test public void testSerialize()
+    {
+        Geocache g = new Geocache("OC1234", "test", new Coordinate(0, 0), 0.0,
+                                  0.0, "Tradi");
+        Geocache g2 = ObjectHelper.copy(g);
+        assertTrue(g2 != null);
+    }
+}

f8ada25fbea7733f5aabd353e88f9d9544366d26
RoffelKartoffel <RoffelKartoffel@users.noreply.github.com>
2017-04-02 00:39:54 +0200

    Add test suite for cmanger.geo

---
+++ test/cmanager/geo/AllTests.java
@@ -0,0 +1,12 @@
+package cmanager.geo;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses(
+    {CoordinateTest.class, GeocacheComparatorTest.class, GeocacheTest.class})
+public class AllTests
+{
+}
